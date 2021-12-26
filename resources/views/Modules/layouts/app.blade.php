62a0c4e4122c4fac33175fe8c83016a282794f78
Hersyanda Putra Adi <49233604+rlmyandaa@users.noreply.github.com>
2021-12-26 16:01:49 +0700

    gitignore

--- resources/views/Modules/layouts/app.blade.php
+++ resources/views/Modules/layouts/app.blade.php
@@ -35,11 +35,7 @@
 
     {{-- Scripts --}}
     <script>
-        window.Laravel = {
-            !!json_encode([
-                'csrfToken' => csrf_token(),
-            ]) !!
-        };
+        window.Laravel = {!!json_encode(['csrfToken' => csrf_token(),]) !!};
     </script>
     <script src="{{ mix('/js/app.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
