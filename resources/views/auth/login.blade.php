4e46b62495cdfaf1823574a5582d32ffa6ee6046
Hersyanda Putra Adi <49233604+rlmyandaa@users.noreply.github.com>
2021-12-27 09:32:37 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -150,7 +150,7 @@
     <nav class="navbar navbar-expand-sm navbar-light fixed-top" id="mainNav" style="background-color: transparent !important;">
         <div class="container">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/">Landing Page</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/" style="color: whitesmoke !important;">Landing Page</a></li>
             </ul>
         </div>
     </nav>
